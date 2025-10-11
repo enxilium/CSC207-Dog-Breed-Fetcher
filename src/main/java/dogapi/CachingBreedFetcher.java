e746279b5e57a2f436f77f36d2b03388280dda91
Jace <jace.zk.dev@gmail.com>
2025-10-11 19:25:10 -0400

    feat: tasks complete

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -13,16 +13,22 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
     private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
+    private Map<String, List<String>> subbreedsMapping = new HashMap<>();
+    private BreedFetcher fetcher;
 
+    public CachingBreedFetcher(BreedFetcher fetcher) {
+        this.fetcher = fetcher;
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        if (!subbreedsMapping.containsKey(breed)) {
+            callsMade++;
+            List<String> subbreeds = fetcher.getSubBreeds(breed); // can throw
+            subbreedsMapping.put(breed, subbreeds);
+        }
+        return subbreedsMapping.get(breed);
     }
 
     public int getCallsMade() {
--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,12 +24,31 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        String url = String.format("https://dog.ceo/api/breed/%s/list", breed);
+        Request request = new Request.Builder().url(url).build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                assert response.body() != null;
+                String message = response.body().string();
+                JSONObject jsonObject = new JSONObject(message);
+                JSONArray jsonArray = jsonObject.getJSONArray("message");
+
+                List<String> subbreeds = new ArrayList<>();
+
+                for (int  i = 0; i < jsonArray.length(); i++) {
+                    String subbreed =  jsonArray.getString(i);
+                    subbreeds.add(subbreed);
+                }
+
+                return subbreeds;
+            }
+            throw new BreedNotFoundException(breed);
+        } catch  (IOException e) {
+            e.printStackTrace();
+        }
+
         return new ArrayList<>();
     }
 }
